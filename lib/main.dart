b5c991ef1d767153c643482a6f6f86137690e4b9
takkilong <53486950+takkilong@users.noreply.github.com>
2025-04-23 12:17:04 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(title: Text('Hairstyle App')),
+    body: Center(child: Text('Flutter v2 Ready')),
+  ),
+));
